5e831bdb2b43ef18f9a1a15cdc9e5ab47d2a1f1b
Zuo Lan <i@zuolan.me>
2026-04-03 20:17:17 +0800

    fix

--- start-openclaw.sh
+++ start-openclaw.sh
@@ -34,6 +34,7 @@ update_gateway_config() {
             | .gateway.mode = "remote"
             | .gateway.bind = "lan"
             | .gateway.remote.url = "ws://127.0.0.1:18789"
+            | .gateway.http.endpoints.chatCompletions.enabled = true
             | .browser.enabled = true
             | .browser.defaultProfile = "openclaw"
             | .browser.headless = true
